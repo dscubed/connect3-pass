4a3f3924fe54a273703b5caf697b1285168a5f85
Michael Ren <37400755+micha31r@users.noreply.github.com>
2026-02-06 00:51:43 +1100

    try fix deployment bug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone',
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'c3-pass-assets.vercel.app',
+            },
+        ],
+    },
+};
 
 module.exports = nextConfig;
