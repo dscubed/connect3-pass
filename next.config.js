5a4f482df2f65eba64562b869934d56346230110
Michael Ren <37400755+micha31r@users.noreply.github.com>
2026-02-06 01:19:55 +1100

    chore: clean up config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // output: 'standalone',
-    // eslint: {
-    //     // Warning: This allows production builds to successfully complete even if
-    //     // your project has ESLint errors.
-    //     ignoreDuringBuilds: true,
-    // },
-    // images: {
-    //     remotePatterns: [
-    //         {
-    //             protocol: 'https',
-    //             hostname: 'c3-pass-assets.vercel.app',
-    //         },
-    //     ],
-    // },
-    // experimental: {
-    //     outputFileTracingIncludes: {
-    //         '/api/**/*': ['./class-template.json'],
-    //     },
-    // },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
